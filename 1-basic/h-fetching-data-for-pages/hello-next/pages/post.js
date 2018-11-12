81c08ce1eb810e0caee2286e2ab9ce34f81cb43a
Ray <ray@amazingco.me>
2018-11-12 15:27:48 +1100

    Fetching Data for Pages
    Here we can only see the message on the browser console.
    
    That's because we navigated to the post page via the client side. Then fetching
    data from the client side is the best way to do it.
    
    Now you have learned one of the most crucial features of Next.js that makes it
    ideal for universal data fetching and server side rendering.
    
    We have learned the basics of getInitialProps which is enough for most of the
    use cases.

--- 1-basic/h-fetching-data-for-pages/hello-next/pages/post.js
+++ 1-basic/h-fetching-data-for-pages/hello-next/pages/post.js
@@ -1,19 +1,23 @@
-import { withRouter } from 'next/router';
 import Layout from '../components/MyLayout.js';
+import axios from 'axios';
 
-const Content = withRouter(
-  (props) => (
-    <div>
-      <h1>{props.router.query.title}</h1>
-      <p>This is the blog post content.</p>
-    </div>
-  )
-)
-
-const Page = (props) => (
+const Post = (props) => (
   <Layout>
-    <Content />
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium} />
   </Layout>
 )
 
-export default Page;
+Post.getInitialProps = async function (context)
+{
+  const { id } = context.query;
+  const response = await axios.get(`https://api.tvmaze.com/shows/${id}`);
+  const show = await response.data;
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show }
+}
+
+export default Post;
